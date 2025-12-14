a04959f7340d5832b7ad7a2ae49c7d5309b5dcc2
YunusEdizer <yunusemreedizer@stu.topkapi.edu.tr>
2025-12-15 00:36:28 +0300

    Add project files.

---
+++ ApiProjectCamp.WebApi/Entities/Image.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiProjectCamp.WebApi.Entities
+{
+    public class Image
+    {
+        public int ImageId { get; set; }
+        public string Tittle { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
